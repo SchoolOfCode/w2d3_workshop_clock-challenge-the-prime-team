f3069a9f444c4085ab0d0bec7b3acdd40b615f40
alexandertelecan <alexandertelecan@gmail.com>
2021-11-24 16:46:20 +0000

    completed assignment

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-const hand = document.querySelector(".hand");
+const handSeconds = document.querySelector(".hand--seconds");
+const handMinutes = document.querySelector(".hand--minutes");
+const handHours = document.querySelector(".hand--hours");
 
-hand.style.transform = `rotate(90deg)`; // example of how to rotate the hand
+let hour = new Date().getHours();
+let minute = new Date().getMinutes();
+let second = new Date().getSeconds(); 
+handSeconds.style.transform = `rotate(${second * 6}deg)`; 
+handMinutes.style.transform = `rotate(${minute * 6}deg)`;
+handHours.style.transform = `rotate(${hour * 6}deg)`;
+
+
+setInterval(() => {
+    const second = new Date().getSeconds();
+    handSeconds.style.transform = `rotate(${second * 6}deg)`; 
+    if (second === 0) {
+        minute = new Date().getMinutes();
+        handMinutes.style.transform = `rotate(${minute * 6}deg)`
+        if (minute === 0) {
+            hour = new Date().getHours();
+            handHours.style.transform = `rotate(${hour * 6}deg)`;
+        }
+    }
+}, 1000)
